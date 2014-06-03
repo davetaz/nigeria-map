762a560418b0cfc0f8e07037eb08f1aa3220753f
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2014-06-03 15:20:34 +0100

    Early map and website

---
+++ README.md
@@ -0,0 +1,28 @@
+Burkina-Faso-map
+================
+
+The objective of this small project is to prepare a GeoJSON representation of Burkina Faso's regions and provinces to be offered by the African country's new open data portal to its users.
+
+##Map format conversion
+
+Conversion from shapefile to GeoJSON was performed using the [ogr2ogr utility](http://www.gdal.org/ogr2ogr.html), part of [GDAL](http://www.gdal.org/index.html), the open source geospatial data abstraction library. 
+
+After unzipping MapMakers' original shapefiles in *data/raw*, Two simple commands are sufficient to perform the conversion:
+
+    ogr2ogr -f data/processed/geoJSON burkinaFaso_regions.json data/raw/BUF-level_1_SHP/BUF-level_1.shp
+    ogr2ogr -f data/processed/geoJSON burkinaFaso_provinces.json data/raw/BUF_admin_SHP/BUF.shp
+
+It is useful to consider the option to make the maps more suitable to bad or slow Internet connectivity by creating smaller GeoJSON files. This is achieved by losing some of the detail in the regions and provinces' boundaries definition, using the *-lco COORDINATE_PRECISION=[target_precision]* to the above commands, where *target_precision* is an integer >=1, e.g.:
+
+    ogr2ogr -lco COORDINATE_PRECISION=1 -f data/processed/geoJSON burkinaFaso_regions.json data/raw/BUF-level_1_SHP/BUF-level_1.shp
+    ogr2ogr -lco COORDINATE_PRECISION=1 -f data/processed/geoJSON burkinaFaso_provinces.json data/raw/BUF_admin_SHP/BUF.shp
+
+The bigger the value used, the less detail is lost in creating the GeoJSON file. See Bjørn Sandvik's great blog post ["How to minify GeoJSON files?"](http://blog.thematicmapping.org/2012/11/how-to-minify-geojson-files.html?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+thematicmapping+%28thematic+mapping+blog%29) for more detail.
+
+##Licence
+
+The original Burkina Faso maps' shapefiles are sourced from [MapMaker Ltd.'s "Map Library" service](http://www.mapmakerdata.co.uk.s3-website-eu-west-1.amazonaws.com/library/index.htm): a source of public domain basic map data concerning administrative boundaries in Africa. MapMaker's sources are described on [this web page](http://www.mapmakerdata.co.uk.s3-website-eu-west-1.amazonaws.com/library/sources.htm) on their website.
+
+![Creative Commons License](http://i.creativecommons.org/l/by/4.0/88x31.png "Creative Commons License") The work of conversion to GeoJSON format and the example interactive Web pages provided with the new files is licensed under the [Creative Commons Attribution 4.0 International License](http://creativecommons.org/licenses/by/4.0/).
+
+![Digital Contraptions Imaginarium's logo](images/dicoim.png) Burkina-Faso-map is a [Digital Contraptions Imaginarium Ltd.](http://www.digitalcontraptionsimaginarium.co.uk/) project for the [Open Data Institute](http://theodi.org/).
---
+++ index.js
@@ -0,0 +1,210 @@
+var	CONFIGURATION = {
+		"layers": {
+			// the order is relevant! from the bottom to the top one
+			"Provinces": {
+				"dataFile": "/data/processed/burkinaFaso_provinces.json",
+				"dataType": "geojson",
+				"colour": "orange",
+			},
+			"Regions": {
+				"dataFile": "/data/processed/burkinaFaso_regions.json",
+				"dataType": "geojson",
+				"colour": "red",
+			},
+		}
+	};	
+
+var configuration,
+	layers = { },
+	map,
+	layersControl,
+	infoControl,
+	titleControl, 
+	zoomControl;
+
+var qs = (function(a) {
+    if (a == "") return {};
+    var b = {};
+    for (var i = 0; i < a.length; ++i)
+    {
+        var p=a[i].split('=');
+        if (p.length != 2) continue;
+        b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    }
+    return b;
+})(window.location.search.substr(1).split('&'));
+
+var onEachFeature = function (feature, layer) {
+
+	var highlightFeature = function (e) {
+	    var layer = e.target;
+	    layer.setStyle({
+	        weight: 5,
+	        color: '#666',
+	        dashArray: '',
+	        fillOpacity: 0.4
+	    });
+	    if (!L.Browser.ie && !L.Browser.opera) {
+	        layer.bringToFront();
+	    }
+		if (!qs.embed) infoControl.update(layer.feature.properties);
+	}
+
+	var resetHighlight = function (e) {
+		// TODO is there a better way of doing this?
+	    _.each(layers, function (layer) { layer.resetStyle(e.target) });
+	    if (!qs.embed) infoControl.update();
+	}
+
+	var zoomToFeature = function (e) {
+	    map.fitBounds(e.target.getBounds());
+	}
+
+	var openLicenceDetail = function (e) {
+		if (!qs.embed) {
+			window.open("https://www.og.decc.gov.uk/eng/fox/decc/PED300X/licence?LICENCE_TYPE=" + e.target.feature.properties["LICENCE_TY"] + "&LICENCE_NO="  + e.target.feature.properties["LICENCE"].match(/\d+/g), "_blank");
+		} else {
+			window.open("https://github.com/Digital-Contraptions-Imaginarium/oil-and-gas-licensing-map", "_blank");
+		}
+	}
+
+	layer.on({
+		mouseover: highlightFeature,
+		mouseout: resetHighlight,
+		click: zoomToFeature,
+		dblclick: openLicenceDetail,
+	});
+
+}
+
+var style = function (feature) {
+    return {
+        fillColor: configuration.layers[feature.properties.licenceType].colour,
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
+
+var initMap = function () {
+
+	_.mixin(_.str.exports());
+
+	configuration = CONFIGURATION;
+	async.each(_.keys(configuration.layers), function (layerName, callback) {
+		switch (configuration.layers[layerName].dataType) {
+			case "geojson":
+				d3.json(configuration.layers[layerName].dataFile, function(data) { 
+					configuration.layers[layerName].geoJSON = data;
+					configuration.layers[layerName].geoJSON.features = _.map(
+						configuration.layers[layerName].geoJSON.features, 
+						function (feature) {
+							feature.properties.licenceType = layerName;
+							return feature;
+						});
+					callback(null); 
+				});
+				break;
+		}
+	}, function (err) {
+
+		// create the tile layer with correct attribution
+		var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+			osmAttrib='DECC <a target="_blank" href="https://www.gov.uk/oil-and-gas-onshore-maps-and-gis-shapefiles">oil and gas licensing data</a> is available under the <a target="_blank" href="http://www.nationalarchives.gov.uk/doc/open-government-licence/version/2/">Open Government Licence v2.0</a> | Map data &copy; <a target="_blank" href="http://www.openstreetmap.org/about">OpenStreetMap</a> contributors' + (!qs.embed ? "" : '| See the full website at <a target="_blank" href="http://dico.im/oil-and-gas-licensing-map/">Digital Contraptions Imaginarium</a>'),
+			osm = new L.TileLayer(osmUrl, { minZoom: 1, maxZoom: 12, attribution: osmAttrib });		
+
+		// set up the data layers
+		_.each(_.keys(configuration.layers), function (layerName) {
+			layers[layerName] = L.geoJson(configuration.layers[layerName].geoJSON, { 
+				/* TODO I have tried defining styling functions in CONFIGURATION 
+				but then layer.resetStyle fails */
+				style: style, 
+				onEachFeature: onEachFeature,
+			});
+		});
+
+		// set up the map
+		var defaultLayersToDisplay = [ osm ];
+		if (qs.regions != "hide") defaultLayersToDisplay = defaultLayersToDisplay.concat(layers["Regions"]);
+		if (qs.provinces != "hide") defaultLayersToDisplay = defaultLayersToDisplay.concat(layers["Provinces"]);
+		map = new L.Map('map', {
+			layers: defaultLayersToDisplay,	
+			center: new L.LatLng(parseFloat(qs.lat) || 54.0, parseFloat(qs.lon) || 1.5),	
+			zoom: parseInt(qs.zoom) || 6,
+			zoomControl: false,
+		});
+
+		if (!qs.embed) {
+			titleControl = L.control({ position: 'topleft' });
+			titleControl.onAdd = function (map) {
+			    this._div = L.DomUtil.create('div', 'titleControl'); 
+			    this._div.innerHTML = "<h1>oil-and-gas-licensing-map</h1><p>This is a map of existing and potential future oil and gas onshore licences for petroleum exploration and production in the UK (not just shale gas), derived from data made available by the Department of Energy and Climate Change. Please read <a href=\"https://github.com/Digital-Contraptions-Imaginarium/oil-and-gas-licensing-map\">here</a> for more information.</p><p>Information is provided \"as is\", without warranty of any kind, express or implied. Don't buy your next house basing your decision on this map! :-)</p>";
+			    return this._div;
+			};
+			titleControl.addTo(map);
+		}
+
+		// set up the 'layers control'
+		var layersForControl = { };
+		_.each(_.keys(layers), function (layerName) {
+			layersForControl[layerName + "&nbsp;<div style='width:10px;height:10px;border:1px solid black;background-color:" + configuration.layers[layerName].colour + ";display:inline-block'></div>"] = layers[layerName];
+		});
+		layersControl = L.control.layers(undefined, layersForControl, { collapsed: false, position: 'topleft' });
+		layersControl.addTo(map);
+
+		// set up the 'info control'
+		if (!qs.embed) {
+
+			infoControl = L.control();
+			infoControl.onAdd = function (map) {
+			    this._div = L.DomUtil.create('div', 'infoControl'); 
+			    this.update();
+			    return this._div;
+			};
+
+			// method that we will use to update the control based on feature properties passed
+			infoControl.update = function (properties) {
+				if (properties) {
+			    	this._div.innerHTML = 
+			    		'<h4>Licensing info</h4>' + 
+			    		_.reduce(_.keys(properties).sort(), function (memo, propertyName) {
+		    				// TODO: there are several GeoJSON features' properties 
+		    				// in the DECC data that have null properties
+			    			if (properties[propertyName] != null) {
+					    		switch (propertyName.toLowerCase()) {
+					    			case "round":
+					    				// just rename the label
+										return memo + "<b>Licensing Round No.</b><br />" + _.capitalize(properties[propertyName].toString().toLowerCase()) + "<br />";
+										break
+									case "licence":
+										return memo + "<b>Licence</b><br />" + _.capitalize(properties[propertyName].toString().toLowerCase()) + "<br />(double-click to see the details on the DECC website)<br />";
+										break;
+					    			case "licence_ty":
+					    				// replace the value with the readable licence type
+										return memo + "<b>Licence Type</b><br />" + LICENCE_TYPES[properties["LICENCE_TY"].toLowerCase()] + "<br />";
+					    				break;
+					    			case "licencetype":
+					    				// do nothing, these are for internal use
+					    				return memo;
+					    				break;
+					    			default:
+										return memo + "<b>" + _.capitalize(propertyName.toLowerCase()) + "</b><br />" + _.capitalize(properties[propertyName].toString().toLowerCase()) + "<br />";
+					    		}
+					    	} else {
+					    		return memo;
+					    	}
+			    		}, "");
+			    } else {
+			    	this._div.innerHTML = '<h4>Licensing info</h4>Hover over the map to select an area of interest' 
+			    }
+			};
+			infoControl.addTo(map);
+		}
+
+		// explicitly adding the zoom control so that it is below the titleControl
+		zoomControl = L.control.zoom().addTo(map);
+
+	});
+}
